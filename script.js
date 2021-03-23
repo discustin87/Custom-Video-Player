c1d6ae5156bd58e7883f8deb7386054205f6748c
Justin Croteau <justinwebdev1@gmail.com>
2021-03-23 16:58:22 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,77 @@
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
+const fullScreen = document.getElementById('full-screen');
+
+// play & pause video
+function toggleVideoStatus() {
+   if (video.paused) {
+       video.play();
+   } else {
+       video.pause();
+   }
+}
+
+// update play/pause icon
+function updatePlayIcon() {
+    if (video.paused) {
+        play.innerHTML = '<i class="fa fa-play fa-2x">';
+    } else {
+        play.innerHTML = '<i class="fa fa-pause fa-2x">';
+    }
+}
+
+// update progress & time stamp
+function updateProgress() {
+    progress.value = (video.currentTime / video.duration) * 100;
+
+    // get min
+    let mins = Math.floor(video.currentTime / 60);
+    if (mins < 10) {
+        mins = '0' + String(mins);
+    }
+
+    // get sec
+    let secs = Math.floor(video.currentTime % 60);
+    if (secs < 10) {
+        secs = '0' + String(secs);
+    }
+
+    timestamp.innerHTML = `${mins}:${secs}`;
+}
+
+// set video time to Progress
+function setVideoProgress() {
+    video.currentTime = (+progress.value * video.duration) / 100;
+}
+
+// set video time to Progress
+function stopVideo() {
+   video.currentTime = 0;
+   video.pause();
+}
+
+// set video time to Progress
+function goToFullScreen() {
+    console.log('no full screen');
+
+  return true
+}
+
+
+
+// EL
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+
+stop.addEventListener('click', stopVideo);
+
+progress.addEventListener('change', setVideoProgress);
+
+fullScreen.addEventListener('click', goToFullScreen);
